9bb96a651349b094e06a1af3045d09ab2a7427e1
2244759 <2244759@cmontmorency.qc.ca>
2024-09-05 16:42:47 -0400

    routage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./containers/Roots";
 import RegisterLogin from "./components/loginRegister/RegisterLogin";
 import offreEmploi from "./components/joboffer/joboffer";
+import PublierOffre from "./components/publierOffre/PublierOffre";
+import MesOffres from "./components/mesOffres/MesOffres";
 import "./App.css";
 
 const router = createBrowserRouter([
@@ -16,8 +18,54 @@ const router = createBrowserRouter([
   },
 ]);
 
+const routerLogin = createBrowserRouter([
+  {
+    path: "/",
+    element: <PublierOffre />,
+    children: [{ path: "/mesOffres", element: <MesOffres /> }],
+  },
+]);
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+  }, []);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  console.log("isLoggedIn", isLoggedIn);
+  if (isLoggedIn) {
+    console.log("User conn.");
+    return (
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      >
+        <RouterProvider router={routerLogin} />
+      </AuthContext.Provider>
+    );
+  } else {
+    console.log("User deco");
+
+    return (
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+      >
+        <RouterProvider router={router} />
+      </AuthContext.Provider>
+    );
+  }
 };
 
 export default App;
