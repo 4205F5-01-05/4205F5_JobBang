d6a292a83e78e0e96c4314472a74158f5c12e005
William Malumba Mudingayi <2156818@cmontmorency.qc.ca>
2024-09-05 15:22:15 -0400

    Gabarit des routes

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
-//import App from "./components/App";
+import App from "./components/App";
 import { AuthProvider } from "./context/auth-context";
 
 const rootElement = document.getElementById("root");
