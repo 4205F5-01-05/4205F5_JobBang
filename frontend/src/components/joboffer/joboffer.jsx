3aff54b233d54679eb5c3b226f1bdc23c325cad8
Will <2156818@cmontmorency.qc.ca>
2024-09-24 14:07:50 -0400

    FAIT: Le bouton supprimer ne doit qu'etre visible que quand l'utilisateur est connecté. Le bouton permet de supprimer l'offre selectionné de l'utilisateur courant

--- frontend/src/components/joboffer/joboffer.jsx
+++ frontend/src/components/joboffer/joboffer.jsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { Box, Typography } from "@mui/material";
+import "./joboffer.css"; // Import the CSS file
 
-const Joboffer = ({ titre, region, description, onDelete }) => {
+export default function Joboffer({ titre, description }) {
   return (
     <div className="joboffer">
-      <h2>{titre}</h2>
-      <p>{region}</p>
-      <p>{description}</p>
-      <button onClick={onDelete}>Supprimer</button>
+      <Box p={2} className="wrappeJobOffer">
+        <Typography variant="subtitle1">{titre}</Typography>
+        <Typography variant="subtitle2">{description}</Typography>
+      </Box>
     </div>
   );
-};
-
-export default Joboffer;
+}
