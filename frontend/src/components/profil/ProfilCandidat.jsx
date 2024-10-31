11b4bc99a006a0e79c05378eda8cb638b07b89ab
Melia Benoit (Melia-Benoit-SSD) <2133252@cmontmorency.qc.ca>
2024-10-31 16:06:00 -0400

    FAIT: 2.7.2: Ajout de l'option de suppression pour le compte candidat dans le frontend. BUG: La redirection vers la page d'accueil ne fonctionne pas.

--- frontend/src/components/profil/ProfilCandidat.jsx
+++ frontend/src/components/profil/ProfilCandidat.jsx
@@ -1,17 +1,44 @@
 // --- IMPORTS ---
 import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useHttpClient } from "../../hooks/http-hook";
 
 import ModalMessageErreur from "../UIElements/ModalMessageErreur";
 import Spinner from "../UIElements/LoadingSpinner";
 
+import "./ProfilCandidat.css";
+
 // --- DEDFAULT FUNCTION ---
 export default function UserProfileC() {
   const [profile, setProfile] = useState(null);
+  const [retour, setRetour] = useState(false);
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
-  const { user, token } = useAuthContext();
+  const { user, token, logout } = useAuthContext();
 
+  // --- SUPPRESSION ---
+  const [btnDel, setBtnDel] = useState(false);
+  const delAccount = async () => {
+    try {
+        const resData = await sendRequest(
+            `http://localhost:5000/api/employees/${user._id}`,
+            "DELETE",
+            null,
+            {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+            }
+        );
+    } catch (e) {
+        console.error(e);
+    } finally {
+        setRetour(true);
+        logout();
+    }
+
+  }
+
+  // --- INFOS PROFIL ---
   useEffect(() => {
     const cId = user._id;
 
@@ -42,6 +69,9 @@ export default function UserProfileC() {
         {isLoading && <Spinner />}
         <ModalMessageErreur message={error} onClose={clearError} />
     </div>
+
+    {retour === true ? (<Navigate to="/offreEmploi" />) : null}
+
     <div>
       <h1>Mon Profil</h1>
       {profile ? (
@@ -63,6 +93,25 @@ export default function UserProfileC() {
         <p>Aucune information sur le profil disponible</p>
       )}
     </div>
+
+    {/* Suppression */}
+    <div 
+        className="btnPopup"
+        onClick={() => (btnDel ? setBtnDel(false) : setBtnDel(true)) }>
+        Supprimer le compte
+    </div>
+
+    {btnDel ? 
+    (
+        <div className="confirm">
+            <p>Voulez-vous vraiment supprimer votre compte de façon permanente?</p>
+            <div className="oui" onClick={delAccount}>Supprimer</div>
+
+        </div>
+    ) : null}
+        
+    
+    
     </>
   );
 };
