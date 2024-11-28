cddf39e48378e80028c34f75f5eca7ae0552aac0
2244759 <2244759@cmontmorency.qc.ca>
2024-11-28 14:33:39 -0500

    3.2.2 ajout champs bd

--- backend/models/Candidatures.js
+++ backend/models/Candidatures.js
@@ -3,31 +3,32 @@ const JobOffers = require("./JobOffers");
 const Employees = require("./Employees");
 
 const candidatureSchema = new mongoose.Schema({
-    nomEmploye: { type: String, required: true },
-    telEmploye: {
-        type: String,
-        required: true,
-        validate: {
-        validator: function (v) {
-            return /\d{10}/.test(v); // validation strucutre numero téléphone
-        },
-        message: (props) => `${props.value} is not a valid phone number!`,
-        },
-    },
-    emailEmploye: {
-        type: String,
-        required: true,
-        lowercase: true,
-        validate: {
-          validator: function (v) {
-            return /^\S+@\S+\.\S+$/.test(v); // validation de la structure du email
-          },
-          message: (props) => `${props.value} is not a valid email!`,
-        },
+  nomEmploye: { type: String, required: true },
+  telEmploye: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /\d{10}/.test(v); // validation strucutre numero téléphone
       },
-    cvFile: { type: String },
-    joId: { type: mongoose.Types.ObjectId, required: true, ref: JobOffers },
-    eId: { type: mongoose.Types.ObjectId, required: true, ref: Employees },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+  emailEmploye: {
+    type: String,
+    required: true,
+    lowercase: true,
+    validate: {
+      validator: function (v) {
+        return /^\S+@\S+\.\S+$/.test(v); // validation de la structure du email
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  cvFile: { type: String },
+  joId: { type: mongoose.Types.ObjectId, required: true, ref: JobOffers },
+  eId: { type: mongoose.Types.ObjectId, required: true, ref: Employees },
+  show: { type: Boolean, default: true }, // Champ booléen pour la visibilité de l'offre
 });
 
 module.exports = mongoose.model("Candidatures", candidatureSchema);
